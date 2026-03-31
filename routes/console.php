a4fcfa46a8c187734361f42d87f06f1e70ea7a2a
Subhash Mac <info.subhashsuthar@gmail.com>
2026-03-31 09:52:26 +0530

    overlaping in the jobs

--- routes/console.php
+++ routes/console.php
@@ -5,6 +5,5 @@ use Illuminate\Support\Facades\Artisan;
 
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command('game:start')->everyThirtySeconds()->withoutOverlapping();
-Schedule::command('game:calculate-result')->everySecond()->withoutOverlapping();
-
+Schedule::command('game:start')->everyThirtySeconds();
+Schedule::command('game:calculate-result')->everySecond();
